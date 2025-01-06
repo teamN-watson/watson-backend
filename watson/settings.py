7596a54652fe6d4068e3cb94d58c04ec85af3d7e
PSG4160 <psg4160@gmail.com>
2025-01-07 08:28:26 +0900

    Fix: Review모델 score 수정

--- watson/settings.py
+++ watson/settings.py
@@ -57,10 +57,11 @@ SIMPLE_JWT = {
 }
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
