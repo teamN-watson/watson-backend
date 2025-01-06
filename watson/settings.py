ce71e98ab496ebecedac4fa7bc6f36c61b23927d
anem1c <lshsean9123@gmail.com>
2025-01-06 11:17:44 +0900

    update requirements.txt

--- watson/settings.py
+++ watson/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third party
+    
+    # my app
+    'accounts',
+    'reviews',
+    'chatbot',
 ]
 
 MIDDLEWARE = [
