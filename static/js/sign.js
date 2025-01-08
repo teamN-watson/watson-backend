ff01b7724947693eb36b65ee87119ec18f9cbf6b
Choi Changgyu <cck981007@gmail.com>
2025-01-08 16:53:27 +0900

    signup page

--- static/js/sign.js
+++ static/js/sign.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     var csrftoken = $("[name=csrfmiddlewaretoken]").val();
 
-    $("div.signForm form").submit(function(e) {
+    $("div.signinForm form").submit(function(e) {
         e.preventDefault();
         user_id = $("input[name='user_id']").val()
         password = $("input[name='password']").val()
@@ -22,12 +22,47 @@ $(document).ready(function() {
             error: function(data){
                 console.log(data)
                 $("span.error_msg").text(data.responseJSON.message)
-
             }
         });
-
-
     })
-    
+    $("div.signupForm form").submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: "/api/account/signup/",
+            headers:{
+                "X-CSRFToken": csrftoken
+            },
+            method: 'POST',
+            data: {
+                "user_id" : $("input[name='user_id']").val(),
+                "password" : $("input[name='password']").val(),
+                "confirm_password" : $("input[name='confirm_password']").val(),
+                "age" : $("input[name='age']").val(),
+                "nickname" : $("input[name='nickname']").val(),
+                "email" : $("input[name='email']").val()
+            },
+            success: function(data){
+                console.log(data)
+                // location.href = "/"
+            },
+            error: function(data){
+                $(".error_msg").text("")
+                if(data.responseJSON){
+                    const keys = Object.keys(data.responseJSON)
+                    for (let i = 0; i < keys.length; i++) {
+                        console.log()
+                        var key = keys[i]
+                        $("p.error_msg."+key).text(data.responseJSON[keys[i]][0])
+                        
+                    }
+
+                }
+                console.log(data)
+                if(data.responseJSON.message && data.responseJSON.message.length){
+                    $("span.error_msg").text(data.responseJSON.message[0])
+                }
+            }
+        });
+    })
     
 });
