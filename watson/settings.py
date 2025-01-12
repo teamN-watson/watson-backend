047bff0d1f737e8ac6689653cabcdb01dafc5d2e
anem1c <lshsean9123@gmail.com>
2025-01-13 07:57:47 +0900

    delete SECRET_KEY

--- watson/settings.py
+++ watson/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-00_8&5m4x*)+o)n_q_dfs3b_8hl1)r$5*0ngxejb^=i*pwfh33'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
