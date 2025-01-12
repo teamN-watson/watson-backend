340c05706d9fbf3813b79064a96a19c811be6368
anem1c <lshsean9123@gmail.com>
2025-01-13 08:33:33 +0900

    hide secret key

--- watson/settings.py
+++ watson/settings.py
@@ -26,7 +26,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-00_8&5m4x*)+o)n_q_dfs3b_8hl1)r$5*0ngxejb^=i*pwfh33"
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
