7cd3b163747ea81919ef3b1833c19fd57ff7b6c1
anem1c <lshsean9123@gmail.com>
2025-01-23 19:59:46 +0900

    fix

--- watson/settings.py
+++ watson/settings.py
@@ -204,3 +204,5 @@ AUTH_USER_MODEL = "accounts.Account"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+SECURE_CONTENT_TYPE_NOSNIFF = False
